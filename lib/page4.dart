1363ea5c4ab2728c02ff001c5ed3cfb6484f6ce0
MonkeyInWind <xlyytcy_pyc@outlook.com>
2019-12-09 22:20:34 +0800

    ListView GridView

--- lib/page4.dart
+++ lib/page4.dart
@@ -3,7 +3,60 @@ import 'package:flutter/material.dart';
 class Page4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('page4')
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('Title1')
+          ),
+          ListTile(
+              leading: Icon(Icons.cached),
+              title: Text('Title2')
+          ),
+          Container(
+            height: 200,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 150,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 150,
+                ),
+                Container(
+                  color: Colors.green,
+                  width: 150,
+                ),
+                Container(
+                  color: Colors.yellow,
+                  width: 150,
+                ),
+              ]
+            )
+          ),
+          Container(
+            height: 200,
+            decoration: BoxDecoration(
+              border: Border.all(width: 1, color: Colors.black)
+            ),
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: List.generate(100, (index) {
+                return Center(
+                  child: Text('Item $index')
+                );
+              })
+            )
+          )
+        ],
+      )
+    );
   }
 }
