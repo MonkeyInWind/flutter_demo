35d45c1e35ed04e75a5b9dea496415670d0aa4ec
MonkeyInWind <xlyytcy_pyc@outlook.com>
2019-12-09 21:16:08 +0800

    page4

--- lib/page3.dart
+++ lib/page3.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:flutter_demo/page4.dart';
 
 class Page3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text('page1')
-        ),
-        body: Stack(
-          children: <Widget>[
-            Center(
-              child: CircularProgressIndicator()
-            ),
-            Center(
-              child: FadeInImage.memoryNetwork(
-                placeholder: kTransparentImage,
-                image: 'https://www.baidu.com/img/bd_logo1.png'
-              )
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('page3')
+      ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: CircularProgressIndicator()
+          ),
+          Center(
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage,
+              image: 'https://www.baidu.com/img/bd_logo1.png'
+            )
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_forward),
+        onPressed: () {
+          Navigator.push(
+            context,
+            new MaterialPageRoute(builder: (context) => Page4()),
+          );
+        }
+      ),
     );
   }
 }
