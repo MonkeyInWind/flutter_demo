beefd88a70bf77414b0ef69ae99c6bdf5182dc7c
MonkeyInWind <xlyytcy_pyc@outlook.com>
2019-12-10 17:12:36 +0800

    page6

--- lib/page5.dart
+++ lib/page5.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/page6.dart';
 
 class Page5 extends StatelessWidget{
   final items = new List<String>.generate(5, (i) => 'Item ${i + 1}');
@@ -64,7 +65,16 @@ class Page5 extends StatelessWidget{
             )
           )
         ]
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_forward),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Page6())
+          );
+        }
+      ),
     );
   }
 }
