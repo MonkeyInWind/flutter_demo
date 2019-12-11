40833bfe44714009744b8e65c87a406bab8cb35c
MonkeyInWind <xlyytcy_pyc@outlook.com>
2019-12-11 19:20:41 +0800

    有状态组件

--- lib/page6.dart
+++ lib/page6.dart
@@ -7,6 +7,66 @@ class Page6 extends StatelessWidget {
       appBar: AppBar(
         title: Text('page6'),
       ),
+      body: ListView(
+        physics: NeverScrollableScrollPhysics(),
+        children: <Widget>[
+          Center (
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+              margin: EdgeInsets.only(
+                top: 20,
+                bottom: 20
+              ),
+            ),
+          ),
+          Row (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container (
+                padding: EdgeInsets.only(
+                  left: 10,
+                  right: 10
+                ),
+                child: MaterialButton (
+                  color: Colors.red,
+                  child: Text('red'),
+                  onPressed: () {
+                    print('red');
+                  },
+                ),
+              ),
+              Container (
+                padding: EdgeInsets.only(
+                    left: 10,
+                    right: 10
+                ),
+                child: MaterialButton (
+                  color: Colors.blue,
+                  child: Text('blue'),
+                  onPressed: () {
+                    print('blue');
+                  }
+                ),
+              ),
+              Container (
+                padding: EdgeInsets.only(
+                  left: 10,
+                  right: 10
+                ),
+                child: MaterialButton (
+                  color: Colors.green,
+                  child: Text('green'),
+                  onPressed: () {
+                    print('green');
+                  }
+                )
+              )
+            ],
+          )
+        ],
+      )
     );
   }
 }
