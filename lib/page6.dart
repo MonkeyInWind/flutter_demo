6a69a7a634c64cf69d5f92438b051a424449a32f
MonkeyInWind <xlyytcy_pyc@outlook.com>
2019-12-11 22:50:13 +0800

    有状态组件demo

--- lib/page6.dart
+++ lib/page6.dart
@@ -7,64 +7,84 @@ class Page6 extends StatelessWidget {
       appBar: AppBar(
         title: Text('page6'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            width: 200,
-            height: 200,
-            color: Colors.red,
-            margin: EdgeInsets.only(
-              top: 20,
-              bottom: 20
-            ),
+      body: BoxChangeColor()
+    );
+  }
+}
+
+class BoxChangeColor extends StatefulWidget {
+  @override
+  _BoxChangeColorState createState() => new _BoxChangeColorState();
+}
+
+class _BoxChangeColorState extends State<BoxChangeColor> {
+  Color color = Colors.red;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 200,
+          height: 200,
+          color: color,
+          margin: EdgeInsets.only(
+            top: 20,
+            bottom: 20
           ),
-          Row (
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container (
-                padding: EdgeInsets.only(
-                  left: 10,
-                  right: 10
-                ),
-                child: MaterialButton (
-                  color: Colors.red,
-                  child: Text('red'),
-                  onPressed: () {
-                    print('red');
-                  },
-                ),
+        ),
+        Row (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container (
+              padding: EdgeInsets.only(
+                left: 10,
+                right: 10
               ),
-              Container (
-                padding: EdgeInsets.only(
-                    left: 10,
-                    right: 10
-                ),
-                child: MaterialButton (
-                  color: Colors.blue,
-                  child: Text('blue'),
-                  onPressed: () {
-                    print('blue');
-                  }
-                ),
+              child: MaterialButton (
+                color: Colors.red,
+                child: Text('red'),
+                onPressed: () {
+                  setState(() {
+                    color = Colors.red;
+                  });
+                },
               ),
-              Container (
-                padding: EdgeInsets.only(
-                  left: 10,
-                  right: 10
-                ),
-                child: MaterialButton (
-                  color: Colors.green,
-                  child: Text('green'),
-                  onPressed: () {
-                    print('green');
-                  }
-                )
+            ),
+            Container (
+              padding: EdgeInsets.only(
+                left: 10,
+                right: 10
+              ),
+              child: MaterialButton (
+                color: Colors.blue,
+                child: Text('blue'),
+                onPressed: () {
+                  setState(() {
+                    color = Colors.blue;
+                  });
+                }
+              ),
+            ),
+            Container (
+              padding: EdgeInsets.only(
+                left: 10,
+                right: 10
+              ),
+              child: MaterialButton (
+                color: Colors.green,
+                child: Text('green'),
+                onPressed: () {
+                  setState(() {
+                    color = Colors.green;
+                  });
+                }
               )
-            ],
-          )
-        ],
-      )
+            )
+          ],
+        )
+      ],
     );
   }
 }
